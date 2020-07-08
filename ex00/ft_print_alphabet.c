2be9083d4b4feea1ad5e514383dbcbc19744b7f4
Nkosinathi Radebe <nkradebe@c4r11s8.wethinkcode.co.za>
2020-07-08 16:24:33 +0200

    commit day02 ex00

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkradebe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 15:50:35 by nkradebe          #+#    #+#             */
+/*   Updated: 2020/07/08 16:22:14 by nkradebe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char prnt)
+{
+	write(1, &prnt, 1);
+}
+
+
+void ft_print_alphabets(void)
+{
+   	char alph;
+	alph = 'a';
+			while(alph >= 'a' && alph <= 'z')
+			{ 
+				ft_putchar(alph);
+				alph += 1;
+			}
+			return;
+
+}
+
